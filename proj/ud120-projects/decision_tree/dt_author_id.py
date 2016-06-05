871608e7aba2942967f3c5083ab5e2f30d17655f
Alan Ning <askldjd@gmail.com>
2016-06-05 13:29:27 -0400

    decision tree lecture

--- proj/ud120-projects/decision_tree/dt_author_id.py
+++ proj/ud120-projects/decision_tree/dt_author_id.py
@@ -25,6 +25,35 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+""" compute the accuracy of your Naive Bayes classifier """
+
+print(len(features_train[0]))
+
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+print('classifier training begin')
+### fit the classifier on the training features and labels
+t0 = time()
+
+clf = clf.fit(features_train, labels_train)
+
+print "training time:", round(time()-t0, 3), "s"
+
+
+### calculate and return the accuracy on the test data
+### this is slightly different than the example,
+### where we just print the accuracy
+### you might need to import an sklearn module
+print('classifier prediction begin')
+t0 = time()
+clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+
+accuracy = clf.score(features_test, labels_test)
+print(accuracy)
+# return accuracy
 
 #########################################################
 
