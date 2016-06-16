30bab047e43c150554d6e24bb74118fb82318a38
Alan Ning <askldjd@gmail.com>
2016-06-15 20:59:54 -0400

    completed dataset question lecture

--- proj/ud120-projects/datasets_questions/explore_enron_data.py
+++ proj/ud120-projects/datasets_questions/explore_enron_data.py
@@ -19,4 +19,24 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+#Wesley Colwell
+# James Prentice
 
+print enron_data["COLWELL WESLEY"]
+
+count = 0.0
+total = 0.0
+for key, value in enron_data.iteritems() :
+    if value["poi"] == True:
+        if enron_data[key]["total_payments"] == "NaN": 
+            #print key, len(value)
+            count+=1
+        total+=1
+
+print count
+print total
+print '%.2f'%(count / total)
+
+#print enron_data["SKILLING JEFFREY K"]["total_payments"]
+#print enron_data["LAY KENNETH L"]["total_payments"]
+#print enron_data["FASTOW ANDREW S"]["total_payments"]
